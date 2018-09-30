1b6bae3499374011f84662d5e33b03fee2b27dac
duongthao1610 <duongthao.1610@gmail.com>
2018-10-01 03:43:04 +0700

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  before_save email_downcase
-  validates :name, presence: true, length: {maximum: Setting.user.max_name_size}
+  before_save :email_downcase
+  validates :name, presence: true, length: {maximum: Settings.user.max_name_size}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Setting.user.max_email_size}, format: {with: VALID_EMAIL_REGEX},
+  validates :email, presence: true, length: {maximum: Settings.user.max_email_size}, format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Setting.mini_password}
+  validates :password, presence: true, length: {minimum: Settings.user.mini_password}
 
   private
 
