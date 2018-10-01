c59f9e47a9c71cb1baf2645a11d2fab01a80f445
duongthao1610 <duongthao.1610@gmail.com>
2018-10-01 18:34:20 +0700

    Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show
     @user= User.find_by id: params[:id]
     if @user.nil?
-      flash[:danger] = t(".user_not_found")
+      flash[:danger] = t ".user_not_found"
       redirect_to root_path
     end
   end
@@ -14,11 +14,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save 
-      flash[:success] = t(".welcome")
+      log_in @user
+      flash[:success] = t ".welcome"
       redirect_to @user
     else 
-      flash[:danger] = t(".failed")
-      render :new  
+      flash[:danger] = t ".failed" 
+      render :new 
     end
   end
 
