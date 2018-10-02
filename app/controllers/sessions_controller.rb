3cf0a0cb70bf3f5bb3e277aa55f209098631250d
duongthao1610 <duongthao.1610@gmail.com>
2018-10-03 02:36:18 +0700

    Advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-  include SessionsHelper 
   
+  include SessionsHelper  
   def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget_user(user)
       redirect_to user
     else 
       flash.now[:danger] = t ".require_login"
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
