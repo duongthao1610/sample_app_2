6a18dde329e4977e420c1eabcf5b09a35667f637
duongthao1610 <duongthao.1610@gmail.com>
2018-10-03 20:33:30 +0700

    Updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,79 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(index edit update)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: %i(new create index)
+
   def new
     @user = User.new
   end
 
-  def show
-    @user= User.find_by id: params[:id]
-    if @user.nil?
-      flash[:danger] = t ".user_not_found"
-      redirect_to root_path
-    end
+  def show; end
+
+  def index
+    @users = User.select_user.page(params[:page])
+      .order_by_created.per Settings.users_controller.per_page
   end
 
   def create
     @user = User.new user_params
-    if @user.save 
+    if @user.save
       log_in @user
       flash[:success] = t ".welcome"
       redirect_to @user
-    else 
-      flash[:danger] = t ".failed" 
-      render :new 
+    else
+      flash[:danger] = t ".failed"
+      render :new
     end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".updated_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".user_deleted"
+    else
+      flash[:danger] = t ".failed"
+    end
+    redirect_to users_url
   end
 
   private
 
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".user_not_found"
+    redirect_to root_url
+  end
+
   def user_params
-    params.require(:user).permit :name, :email, :password, :password_confirmation
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
