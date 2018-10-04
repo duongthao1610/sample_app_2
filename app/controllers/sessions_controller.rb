f53edb37fd29299293810fd045dfd91ef0238c15
duongthao1610 <duongthao.1610@gmail.com>
2018-10-05 02:18:24 +0700

    Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < ApplicationController
-  
   def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget_user(user)
-      session[:forwarding_url]
-      redirect_back_or user
-    else 
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) :  forget_user(user)
+        session[:forwarding_url]
+        redirect_back_or user
+      else
+        flash[:warning] = t ".message"
+        redirect_to root_url
+      end
+    else
       flash.now[:danger] = t ".require_login"
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,22 @@
 class User < ApplicationRecord
-  attr_accessor:remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :email_downcase
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.user.max_name_size}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.user.max_email_size}, format: {with: VALID_EMAIL_REGEX},
+  validates :email, presence: true, length: {maximum: Settings.user.max_email_size},
+    format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.user.mini_password}
+  validates :password, presence: true,
+    length: {minimum: Settings.user.mini_password}, allow_nil: true
 
   scope :select_user, -> {select :id, :email, :name}
   scope :order_by_created, -> {order created_at: :desc}
 
   def User.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? bCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ?
+      bCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
   end
 
@@ -25,18 +29,33 @@ class User < ApplicationRecord
     update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = self.send("#{attribute}_digest")
+
+    return false unless !digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update remember_digest: nil
   end
 
+  def activate
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def email_downcase
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
